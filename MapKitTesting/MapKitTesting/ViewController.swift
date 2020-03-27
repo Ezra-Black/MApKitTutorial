581fcaf5d19729653f599931b87145c3577473c2
Ezra Black <dev.ezrablack@gmail.com>
2020-03-27 14:39:42 -0500

    Base Project Boiler Plate

---
+++ MapKitTesting/MapKitTesting/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MapKitTesting
+//
+//  Created by Joseph Rogers on 3/27/20.
+//  Copyright © 2020 Casanova Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
